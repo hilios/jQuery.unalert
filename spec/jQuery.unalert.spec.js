914518ef7651f4406d9817ecc841f62bdb736b5e
Edson Hilios <ehilios@corp.hotwords.com>
2011-12-02 17:17:35 -0200

    Initiating the real implementation of the plugin

--- spec/jQuery.unalert.spec.js
+++ spec/jQuery.unalert.spec.js
@@ -1,19 +1,102 @@
-describe("jquery.countdown", function() {
-  var $selector,
-      callback = jasmine.createSpy('callback');
-      
-  function setCountdown() {
-    var toDate = arguments.length == 0 ? new Date() : arguments[0];
-    $selector.countdown(toDate, callback);
+describe("jquery.unalert", function() {
+  var id = 0,
+      $selector,
+      callbacks = {
+        show: jasmine.createSpy('showCallback'),
+        hide: jasmine.createSpy('hideCallback'),
+        click: jasmine.createSpy('clickCallback'),
+        align: jasmine.createSpy('alignCallback')
+      };
+
+  function createDomElement(type, message) {
+    if(type)    type = '';
+    if(message) message = '';
+    return $('<div id="alert'+(++id)+'" class="flash '+type+'">'+message+'</div>').appendTo('body');
+  }
+
+  function updateSelector() {
+    $selector = $('div.flash');
   }
   
   beforeEach(function() {
-    $selector = $('<div id="clock" />').appendTo('body');  
+    createDomElement(null, 'Fake message'); // Create at least one!
+    updateSelector();
   });
   
   afterEach(function() {
+    updateSelector();
     $selector.remove();
-    callback.reset();
+    for(var k in callbacks) {
+      callbacks[k].reset();
+    }
   });
-  
+
+  it('should create the DOM used for the test', function() {
+    var node = document.getElementById($selector.first().attr('id'));
+    expect(node).toBeDefined();
+    expect(node).not.toBeNull();
+    expect($selector.length).toBe(1);
+  });
+
+  describe('class methods', function() {
+    it('should have the unalert function on jquery namespace', function() {
+      expect($.unalert).toBeDefined();
+    });
+
+    describe('version', function() {
+      it('should return the version of the plugin as string x.x.x', function() {
+        expect($.unalert.version).toBeDefined();
+        expect($.unalert.version).toMatch(/^[0-9\.beta]+$/gmi)
+      });
+    });
+    describe('settings', function() {
+      it('should return the default settings object', function() {
+        expect($.unalert.settings).toBeDefined();
+      });
+    });
+    describe('defaults', function() {
+      it('should set the settings into the plguin', function() {
+        expect($.unalert.defaults).toBeDefined();
+        $.unalert.defaults({a: 'b'})
+        expect($.unalert.settings.a).toBe('b')
+      });
+      it('should extend the setting object not overwrite it', function() {
+        $.unalert.defaults({top: 'b'});
+        expect($.unalert.settings.top).toBe('b');
+        expect($.unalert.settings.left).toBeDefined();
+      });
+    });
+  });
+
+  describe('instace methods', function() {
+    it('should have the unalert function on jquery selectors namespace', function() {
+      expect($.fn.unalert).toBeDefined();
+    });
+    
+    it('should assign the unalert to any selector', function() {
+      var selector = createDomElement();
+      spyOn(selector, 'unalert');
+      selector.unalert();
+      expect(selector.unalert).toHaveBeenCalled();
+    });
+
+    describe('init', function() {
+      it('should create a reference to the unalert div created', function() {
+        $selector.unalert();
+        expect($selector.data('unalert')).toBeDefined();
+        expect($selector.data('unalert')).not.toBeNull();
+      });
+    });
+
+    describe('show', function() {
+      
+    });
+    describe('hide', function() {});
+    describe('click', function() {});
+    describe('align', function() {});
+    describe('toggle', function() {});
+    describe('update', function() {});
+    describe('destroy', function() {});
+    describe('visible', function() {});
+  });
 });
--- src/jQuery.unalert.js
+++ src/jQuery.unalert.js
@@ -24,6 +24,108 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 (function($) {
-  $.unalert = {version: '1.0.0 beta'};
-  //...
+  var index = 0,
+      unalerts = [];
+
+  function init(options) {
+    // Merge the options passed wih the plugin settings
+    options = $.extend({}, $.unalert.settings, options);
+    return this.each(function() {
+      var $this = $(this).detach(),
+          $unalert = $('<div />').addClass('unalert').attr('id', 'unalert_overlay_'+(++index));
+          $unalert.append($this).appendTo('body');
+      // Set the internal data
+      $unalert.data('count', index);
+      $unalert.data('element', this);
+      $unalert.data('visible', false);
+      $unalert.data('options', options);
+      // Style it
+      $alert.css('position', 'absolute');
+      // $alert.css('top', options.top);
+      // $alert.css('left', options.left);
+      // $alert.css('width', options.width);
+      // $alert.css('height', options.height);
+      $alert.css('z-index', 999999999999999);
+      // Bind it to align into the window
+      $(window).bind('resize.unalert', alignHandler).
+                bind('scroll.unalert', alignHandler);
+      $(window).resize();
+      // Add reference to the plugin
+      unalerts.push($unalert);
+    });
+  }
+
+  function alignHandler() {
+    var windowWidth     = $(window).width(),
+        windowHeight    = $(window).height()
+        windowScrollTop = $(window).scrollTop();
+    // Calculate the alignment
+    $.each(alerts, function(index, $alert) {
+      var options = $alert.data('options'),
+          element = $alert.data('element')
+          horizontalRatio, verticalRatio;
+      // Left margin
+      if(horizontalRatio = getRatio(options.left)) {
+        var leftMarginOffset = $alert.width() * horizontalRatio * -1;
+        $alert.css('margin-left', leftMarginOffset);
+      }
+      // Top margin
+      if(verticalRatio = getRatio(options.top)) {
+        var topOffset = windowScrollTop + windowHeight * verticalRatio,
+            topMarginOffset = $alert.height() * verticalRatio / -2;
+        $alert.css('margin-top', topMarginOffset);
+        $alert.css('top', topOffset);
+      }
+      // Execute the callback
+      options.align.call(element);
+    });
+  }
+
+  function showHandler() {
+  }
+
+  function hideHandler() {
+  }
+
+  function clickHandler() {
+  }
+
+  function timeoutHandler() {
+  }
+
+  var methods = {
+    init: init
+  }
+
+  $.unalert = {
+    version: '1.0.0beta',
+    settings: {
+      top: '45%',
+      left: '50%',
+      right: null,
+      bottom: null,
+      width: null,
+      height: null,
+      show: showHandler,
+      hide: hideHandler,
+      click: clickHandler,
+      align: function() {},
+      timeout: timeoutHandler,
+      timespan: 2 * 60 * 1000,
+      visible: true
+    },
+    defaults: function(options) {
+      $.extend($.unalert.settings, options);
+    }
+  };
+
+  $.fn.unalert = function(method) {
+    if(methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if(typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method %s does not exist on jQuery.tooltip'.replace(/\%s/gi, method));
+    }
+  };
 })(jQuery)
